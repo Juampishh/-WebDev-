f61c56c9a75931fb6dc56945286be41ee43d90d7
JuampMerino-767 <juampimerino767@gmail.com>
2023-04-27 15:05:59 -0300

    Subiendo PageCrafter

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Navbar />;
+}
+
+export default App;
---
+++ src/components/Navbar/NavbarStyled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const NavbarContainerStyled = styled.header`
+  background-color: #000;
+  width: 100%;
+  height: 100px;
+`;
